50122b7a197ecd449746dfc2a11a527f7ff55c0e
Wei Yang <refeiner@hotmail.com>
2016-09-14 20:02:49 +0800

    how to build test

---
+++ CloudTestConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Cloud Test!");
+            Console.Read();
+        }
+    }
+}
